609f9b52a76fc411009df06d8266da1bfe626f1b
Tyby Boiciuc <boiciuctyby27@gmail.com>
2026-02-20 12:42:36 +0200

    updated

--- src/prompts/__init__.py
+++ src/prompts/__init__.py
@@ -1,33 +1,6 @@
 # Prompts package
-from .conv_analyzer_prompts import CONV_ANALYZER_SYSTEM_PROMPT, USER_PROMPT_TEMPLATE
-from .response_synthesizer_prompts import (
-    SYNTHESIZER_PROMPT, 
-    NO_RETRIEVAL_PROMPT, 
-    PRODUCT_RECOMMENDATION_PROMPT
-)
-from .agentic_prompts import (
-    SAFETY_RULES,
-    ENTITY_RULES,
-    CONVERSATION_PATTERNS,
-    REACT_THINKING_PROMPT,
-    RESPONSE_SYNTHESIS_PROMPT,
-    format_thinking_prompt,
-    format_response_prompt,
-    format_product_prompt,
-    format_no_retrieval_prompt
-)
+from .react_agent_prompt import REACT_SYSTEM_PROMPT
 
 __all__ = [
-    "CONV_ANALYZER_SYSTEM_PROMPT",
-    "USER_PROMPT_TEMPLATE",
-    "SYNTHESIZER_PROMPT",
-    "NO_RETRIEVAL_PROMPT",
-    "PRODUCT_RECOMMENDATION_PROMPT",
-    "SAFETY_RULES",
-    "ENTITY_RULES",
-    "CONVERSATION_PATTERNS",
-    "format_thinking_prompt",
-    "format_response_prompt",
-    "format_product_prompt",
-    "format_no_retrieval_prompt"
+    "REACT_SYSTEM_PROMPT"
 ]
