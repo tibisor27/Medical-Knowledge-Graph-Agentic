9036991da77ac3364faa32c095de5f4c4f672d29
Tyby Boiciuc <boiciuctyby27@gmail.com>
2026-02-28 09:24:33 +0200

    updated package import

--- src/agent/__init__.py
+++ src/agent/__init__.py
@@ -1,14 +1,9 @@
-from src.agent.graph import (
-    MedicalChatSession,
-    create_medical_react_agent,
-    get_medical_agent,
-)
-
+from src.agent.session import MedicalAgent
+from src.agent.agent_factory import get_medical_agent
 from src.agent.tools import get_tools
 
 __all__ = [
-    "MedicalChatSession",
-    "create_medical_react_agent",
+    "MedicalAgent",
     "get_medical_agent",
     "get_tools",
 ]
