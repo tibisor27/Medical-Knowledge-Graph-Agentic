5630a756d61c854c35e31a1318aab312d3d03bd8
Tyby Boiciuc <boiciuctyby27@gmail.com>
2026-03-12 14:57:39 +0200

    modularized agent state

---
+++ src/multi_agent/state/__init__.py
@@ -0,0 +1,9 @@
+from .agent_state import MultiAgentState
+from .reducers import clearable_add
+from .utils import log_state_summary
+
+__all__ = [
+    "MultiAgentState",
+    "clearable_add",
+    "log_state_summary",
+]
