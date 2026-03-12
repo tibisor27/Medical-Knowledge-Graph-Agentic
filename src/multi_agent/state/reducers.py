4b31a837439a7ec70196128344a47e9bbcbfc93d
Tyby Boiciuc <boiciuctyby27@gmail.com>
2026-03-12 14:56:57 +0200

    reducer fucntion for graph state cleaning before every agent graph flow

---
+++ src/multi_agent/state/reducers.py
@@ -0,0 +1,9 @@
+def clearable_add(existing_list: list, new_elements: list) -> list:
+
+    if new_elements == ["CLEAR"]:
+        return []
+    
+    if existing_list is None:
+        return new_elements
+        
+    return existing_list + new_elements
