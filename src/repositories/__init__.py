64afa7f1cf15e50b8a9e276d8e4818e7d0700dfa
Tyby Boiciuc <boiciuctyby27@gmail.com>
2026-04-29 09:23:43 +0300

    implemented repository pattern to handle db operations

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -3,11 +3,13 @@ from .neo4j_medication_repository import get_neo4j_medication_repository
 from .neo4j_nutrient_repository import get_neo4j_nutrient_repository
 from .neo4j_symptom_repository import get_neo4j_symptom_repository
 from .neo4j_query_repository import get_neo4j_query_repository
+from .neo4j_belife_product_repository import get_neo4j_product_repository
 
 __all__ = [
     "BaseRepository",
     "get_neo4j_medication_repository",
     "get_neo4j_nutrient_repository",
     "get_neo4j_symptom_repository",
-    "get_neo4j_query_repository"
+    "get_neo4j_query_repository",
+    "get_neo4j_product_repository"
 ]
